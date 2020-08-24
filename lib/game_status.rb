8138692e052c449c179b4b55cca002aca284feae
julio hernandez <julioandres72@gmail.com>
2020-08-24 22:43:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,6 +34,6 @@ def won?(board)
       return false
     end
 
-    def full?(board)
-      board.all? {|index| index == "X" || index == "O"}
-    end
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
