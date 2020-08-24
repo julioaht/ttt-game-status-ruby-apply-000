76435790adca812b391fe732424248db9c2ac44b
julio hernandez <julioandres72@gmail.com>
2020-08-24 22:07:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,8 +16,20 @@ WIN_COMBINATIONS = [
 
 ]
 def won?(board)
-  WIN_COMBINATIONS.each {|win|}
-  index_0 = win[0]
-  index_1 =
-end
+  WIN_COMBINATIONS.each {|win|
+    index_0 = win[0]
+    index_1 = win[1]
+    index_2 = win[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
